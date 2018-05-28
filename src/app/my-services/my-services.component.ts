ae92f912af8959b82250926768c32c3798260b49
andrey104 <andreyrs104@gmail.com>
2018-05-29 01:27:09 +0300

    add helper

--- src/app/my-services/my-services.component.ts
+++ src/app/my-services/my-services.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import {log} from 'util';
 import {ServiceService} from '../services/service.service';
 import {Service} from '../models/service';
+import {AuthService} from "../services/auth.service";
+import {User} from "../models/user";
 
 @Component({
   selector: 'app-my-services',
@@ -10,14 +12,17 @@ import {Service} from '../models/service';
 })
 export class MyServicesComponent implements OnInit {
 
-  constructor(private service: ServiceService) { }
+  constructor(private service: ServiceService,
+              private auth: AuthService) { }
   services: Service[];
+  user: User;
   ngOnInit() {
+    this.user = this.auth.getAuthUser();
     this.load();
   }
 
   load(): void {
-    this.service.getServices()
+    this.service.getProviderServices(this.user.id)
       .subscribe(services => {
         this.services = services;
       }, error2 => {
